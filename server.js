3d6a537628be79c55bbf1a45bad62cfb5da515f8
bolheinmin <bolheinmin@gmail.com>
2020-03-27 18:46:13 +0630

    deploy

--- server.js
+++ server.js
@@ -1,78 +1,17 @@
 
-const 
-request = require('request'),
-express = require('express'),
-bodyParser = require('body-parser'), 
-app = express(); 
-path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(express.static('dist/new-hope'));
+const app = express();
 
-// app.get('*', (req, res) => {
-//   var options = {
-//     root: path.join(__dirname, 'dist/new-hope')
-//   }
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/new-hope'));
 
-//   return res.sendFile('index.html', options);
-// });
-
-
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-// Create a router that can accept JSON
-var router = express.Router();
-router.use(bodyParser.json());
-
-// Accepts POST requests at /webhook endpoint
-router.route('/webhook')
-      .post((req, res) => {  
-
-  // Parse the request body from the POST
-  let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-    body.entry.forEach(function(entry) {
-
-      let webhook_event = entry.messaging[0];
-      let sender_psid = webhook_event.sender.id; 
-
-      if (webhook_event.message) {
-        if(webhook_event.message.quick_reply){
-            handleQuickReply(sender_psid, webhook_event.message.quick_reply.payload);
-          }else{
-            handleMessage(sender_psid, webhook_event.message);                       
-          }                
-      } else if (webhook_event.postback) {        
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-      
-    });
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-})// Accepts GET requests at the /webhook endpoint
-.get((req, res) => {
-  
-
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;  
-
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];  
+app.get('/*', function(req,res) {
     
-  // Check token and mode
-  if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      res.status(200).send(challenge);    
-    } else {      
-      res.sendStatus(403);      
-    }
-  }
+res.sendFile(path.join(__dirname+'/dist/new-hope/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
