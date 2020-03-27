cbda8ff3786817d0719f778c63194f9e09363aeb
bolheinmin <bolheinmin@gmail.com>
2020-03-27 19:03:34 +0630

    deploy

--- server.js
+++ server.js
@@ -13,5 +13,190 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/new-hope/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+
+      let webhook_event = entry.messaging[0];
+      let sender_psid = webhook_event.sender.id; 
+
+      if (webhook_event.message) {
+        if(webhook_event.message.quick_reply){
+            handleQuickReply(sender_psid, webhook_event.message.quick_reply.payload);
+          }else{
+            handleMessage(sender_psid, webhook_event.message);                       
+          }                
+      } else if (webhook_event.postback) {        
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+  
+
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;  
+
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];  
+    
+  // Check token and mode
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      res.status(200).send(challenge);    
+    } else {      
+      res.sendStatus(403);      
+    }
+  }
+});
+
+/**********************************************
+Function to Handle when user send quick reply message
+***********************************************/
+
+function handleQuickReply(sender_psid, received_message) {
+  
+  switch(received_message) {        
+        case "on":
+            //showQuickReplyOn(sender_psid);
+          break;
+        case "off":
+            //showQuickReplyOff(sender_psid);
+          break;                
+        default:
+            //defaultReply(sender_psid);
+  } 
+ 
+}
+
+/**********************************************
+Function to Handle when user send text message
+***********************************************/
+
+const handleMessage = (sender_psid, received_message) => {
+  //let message;
+  let response;
+
+  if(received_message.attachments){
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes-attachment",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no-attachment",
+              }
+            ],
+          }]
+        }
+      }
+    }
+    callSend(sender_psid, response);
+  }
+}
+
+/*********************************************
+Function to handle when user click button
+**********************************************/
+const handlePostback = (sender_psid, received_postback) => {
+  let payload = received_postback.payload;
+  switch(payload) {        
+      case "yes":
+          //showButtonReplyYes(sender_psid);
+        break;
+      case "no":
+          //showButtonReplyNo(sender_psid);
+        break;                
+      default:
+          //defaultReply(sender_psid);
+  } 
+}
+
+const callSendAPI = (sender_psid, response) => {  
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  
+  return new Promise(resolve => {
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        resolve('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  });
+}
+
+async function callSend(sender_psid, response){
+  let send = await callSendAPI(sender_psid, response);
+  return 1;
+}
+
+/***********************************
+FUNCTION TO ADD WHITELIST DOMAIN
+************************************/
+
+const whitelistDomains = (res) => {
+  var messageData = {
+          "whitelisted_domains": [
+             "https://new-hope-ecommerce.herokuapp.com" , 
+             "https://herokuapp.com"                           
+          ]               
+  };  
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      form: messageData
+  },
+  function (error, response, body) {
+      if (!error && response.statusCode == 200) {          
+          res.send(body);
+      } else {           
+          res.send(body);
+      }
+  });
+} 
